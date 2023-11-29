43aa3e1e5c4af1a459e4d1ba6c38ec9ff4ad41d3
Taylor Bisset <taylorbisset@gmail.com>
2023-11-29 14:07:42 -0800

    Added class Node

--- CS132-ExtraCredit-LinkedListLab/main.cpp
+++ CS132-ExtraCredit-LinkedListLab/main.cpp
@@ -1,6 +1,20 @@
 // main.cpp
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node 
+{
+public:
+    string data;
+    Node* next;
+    Node* prev;
+
+    Node(string value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
 
 int main()
 {
