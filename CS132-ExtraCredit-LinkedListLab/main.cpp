6d1c62cc467a3583a5fca19b1026d12500aab1f8
Taylor Bisset <taylorbisset@gmail.com>
2023-12-01 15:08:03 -0800

    Add main()

--- CS132-ExtraCredit-LinkedListLab/main.cpp
+++ CS132-ExtraCredit-LinkedListLab/main.cpp
@@ -124,5 +124,23 @@ public:
 
 int main()
 {
-    cout << "Hello World!\n";
+    DblLinkedList list;
+    list.push_back("1"); // insert back...into empty list
+    list.push_back("2"); // insert back
+    list.push_back("3");
+    list.push_back("4");
+    list.push_back("5");
+    list.push_back("6");
+    list.push_back("7");
+    list.push_back("8");
+    list.push_back("9");
+    list.push_back("10");
+    list.resetIterator();
+    while (list.hasMore()) 
+    {
+        cout << list.next() << endl;
+    }
+    list.testConnections();
+    system("pause");
+    return 0;
 }
